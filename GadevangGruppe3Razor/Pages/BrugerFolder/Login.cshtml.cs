111fb3afe9e440b673cfa858ef2de70d4d3b02e0
VixonyAn <vixonyan@gmail.com>
2025-05-05 14:07:25 +0200

    Login basics

---
+++ GadevangGruppe3Razor/Pages/BrugerFolder/Login.cshtml.cs
@@ -0,0 +1,61 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using GadevangGruppe3Razor.Interfaces;
+using GadevangGruppe3Razor.Services;
+using GadevangGruppe3Razor.Models;
+namespace GadevangGruppe3Razor.Pages.BrugerFolder
+{
+    public class LoginModel : PageModel
+    {
+        #region Instance Fields
+        private IBrugerService _brugerService;
+        #endregion
+
+        #region Properties
+        [BindProperty] public Bruger Bruger { get; set; }
+        public string MessageError { get; set; }
+        #endregion
+
+        #region Constructors
+        public LoginModel(IBrugerService brugerService)
+        {
+            _brugerService = brugerService;
+        }
+        #endregion
+
+        #region Methods
+        public async Task OnGetAsync() { }
+
+        public IActionResult OnGetLogout()
+        {
+            HttpContext.Session.Remove("Email");
+            return RedirectToPage("Login");
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            // if ModelState is NOT valid, reload (triggers error messages)
+            if (!ModelState.IsValid) { return Page(); }
+            try
+            {
+                Bruger? loginBruger = await _brugerService.ValidateBrugerAsync(Bruger.Email, Bruger.Adgangskode);
+                if (loginBruger != null)
+                {
+                    HttpContext.Session.SetString("Email", loginBruger.Email);
+                    return RedirectToPage("Index");
+                }
+                else
+                { // if user cannot be verified
+                    MessageError = "Invalid email eller adgangskode";
+                    return Page();
+                }
+            }
+            catch (Exception ex)
+            { // catches exceptions like weak connection or mistake in query
+                ViewData["ErrorMessage"] = ex.Message;
+                return Page();
+            }
+        }
+        #endregion
+    }
+}
