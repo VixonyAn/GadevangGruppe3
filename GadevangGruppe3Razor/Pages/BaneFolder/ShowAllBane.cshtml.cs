343f94a060a54e373ff53e160d0ed32d94ecfe44
Johanne Marie Dehn Nielsen <jon010@edu.zealand.dk>
2025-05-12 09:52:36 +0200

    Admin check på show alle baner+ tilpasset adnagng til knapper

--- GadevangGruppe3Razor/Pages/BaneFolder/ShowAllBane.cshtml.cs
+++ GadevangGruppe3Razor/Pages/BaneFolder/ShowAllBane.cshtml.cs
@@ -1,5 +1,6 @@
 using GadevangGruppe3Razor.Interfaces;
 using GadevangGruppe3Razor.Models;
+using GadevangGruppe3Razor.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -8,25 +9,42 @@ namespace GadevangGruppe3Razor.Pages.BaneFolder1
     public class ShowAllBaneModel : PageModel
     {
         IBaneService _baneservice;
+        IBrugerService _brugerService;
 
         public List<Bane> Baner { get; set; }
 
-        public ShowAllBaneModel(IBaneService baneservice)
+        [BindProperty]public Bruger CurrentBruger { get; set; }
+
+        public string Email { get; set; }
+
+        public ShowAllBaneModel(IBaneService baneservice,IBrugerService brugerService)
         {
             _baneservice = baneservice;
+            _brugerService = brugerService;
         }
 
-        public async Task OnGetAsync()
+        public async Task<IActionResult> OnGetAsync()
         {
-            try 
-            { 
-                Baner=await _baneservice.GetAllBaneAsync();
-            } 
+            try
+            {
+                Email = HttpContext.Session.GetString("Email");
+                if (Email == null)
+                {
+                    return RedirectToPage("/BrugerFolder/Login");
+                }
+                else
+                {
+                    CurrentBruger = await _brugerService.GetBrugerByEmailAsync(Email);
+                }
+                Baner= await _baneservice.GetAllBaneAsync();
+            }
+            
             catch (Exception ex)
             {
                 List <Bane> baner = new List<Bane>();
                 ViewData["ErrorMessage"] = ex.Message;
             }
+            return Page();
             
         }
     }
