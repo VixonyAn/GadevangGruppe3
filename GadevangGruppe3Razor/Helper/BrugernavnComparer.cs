197c322e9ae60400127a7ce80c27fe2a8baeba01
Magnus Sverdrup <magger254@gmail.com>
2025-05-13 14:28:03 +0200

    Added Sorting to ShowAllBruger

---
+++ GadevangGruppe3Razor/Helper/BrugernavnComparer.cs
@@ -0,0 +1,25 @@
+﻿using GadevangGruppe3Razor.Models;
+
+namespace GadevangGruppe3Razor.Helper
+{
+	public class BrugernavnComparer : IComparer<Bruger>
+	{
+		public int Compare(Bruger? x, Bruger? y)
+		{
+			if (x == null && y == null)
+			{
+				return 0;
+			}
+			else if (x == null)
+			{
+				return -1;
+			}
+			else if (y == null)
+			{
+				return 1;
+			}
+
+			return string.Compare(x.Brugernavn, y.Brugernavn);
+		}
+	}
+}
