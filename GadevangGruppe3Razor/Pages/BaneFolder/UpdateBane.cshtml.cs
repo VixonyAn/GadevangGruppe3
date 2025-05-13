3107e7ef4022b3d2b8cb56b04c3703e46e47864c
Johanne Marie Dehn Nielsen <jon010@edu.zealand.dk>
2025-05-13 09:47:10 +0200

    Selectlist i opdate

--- GadevangGruppe3Razor/Pages/BaneFolder/UpdateBane.cshtml.cs
+++ GadevangGruppe3Razor/Pages/BaneFolder/UpdateBane.cshtml.cs
@@ -2,6 +2,7 @@ using GadevangGruppe3Razor.Interfaces;
 using GadevangGruppe3Razor.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace GadevangGruppe3Razor.Pages.BaneFolder1
 {
@@ -11,10 +12,36 @@ namespace GadevangGruppe3Razor.Pages.BaneFolder1
 
         [BindProperty]
         public Bane bane { get; set; }
+
+        public List<SelectListItem> TypeSelectList { get; set; }
+
+        public List<SelectListItem> MiljøSelectList { get; set; }
+
         public UpdateBaneModel(IBaneService baneservice)
         {
             _baneService = baneservice;
+            CreateBaneTypeSelectList();
+            CreateBaneMiljøSelectList();
         }
+
+        public async void CreateBaneTypeSelectList()
+        {
+            TypeSelectList = new List<SelectListItem>();
+            TypeSelectList.Add(new SelectListItem("Vælg en type", "-1"));
+            TypeSelectList.Add(new SelectListItem("Tennis", "Tennis"));
+            TypeSelectList.Add(new SelectListItem("Paddel", "Paddel"));
+
+
+        }
+
+        public void CreateBaneMiljøSelectList()
+        {
+            MiljøSelectList = new List<SelectListItem>();
+            MiljøSelectList.Add(new SelectListItem("Vælg Miljø", "-1"));
+            MiljøSelectList.Add(new SelectListItem("Udendørs", "Udendørs"));
+            MiljøSelectList.Add(new SelectListItem("Indendørs", "Indendørs"));
+        }
+
         public async Task<IActionResult> OnGetAsync(int baneId)
         {
             bane= await _baneService.GetBaneByIdAsync(baneId);
