7a95d6aa277db8338cdd379f2e25a1d3508c43ae
Magnus Sverdrup <magger254@gmail.com>
2025-05-20 08:58:23 +0200

    Hold Service CRUD done

--- GadevangGruppe3Razor/Services/HoldService.cs
+++ GadevangGruppe3Razor/Services/HoldService.cs
@@ -1,6 +1,7 @@
 ﻿using GadevangGruppe3Razor.Interfaces;
 using GadevangGruppe3Razor.Models;
 using Microsoft.Data.SqlClient;
+using System.Data;
 
 namespace GadevangGruppe3Razor.Services
 {
@@ -51,19 +52,135 @@ namespace GadevangGruppe3Razor.Services
 			return isCreated;
 		}
 
-		public Task<Hold?> DeleteHoldAsync(int holdId)
+		public async Task<Hold?> DeleteHoldAsync(int holdId)
 		{
-			throw new NotImplementedException();
+			Hold? hold = await GetHoldByIdAsync(holdId);
+			if (hold == null)
+			{
+				return null;
+			}
+			using (SqlConnection connection = new SqlConnection(connectionString))
+			{
+				try
+				{
+					SqlCommand command = new SqlCommand(deleteSql, connection);
+					command.Parameters.AddWithValue("@HoldId", holdId);
+					await connection.OpenAsync();
+					int noOfRows = command.ExecuteNonQuery();
+					if (noOfRows == 0)
+					{
+						return null;
+					}
+				}
+				catch (SqlException sqlex)
+				{
+					Console.WriteLine(sqlex.Message);
+					throw sqlex;
+				}
+				catch (Exception ex)
+				{
+					Console.WriteLine(ex.Message);
+					throw ex;
+				}
+				finally
+				{
+
+				}
+				return hold;
+			}
 		}
 
-		public Task<List<Hold>> GetAllHoldAsync()
+		public async Task<List<Hold>> GetAllHoldAsync()
 		{
-			throw new NotImplementedException();
+			List<Hold> holdListe = new List<Hold>();
+			using (SqlConnection connection = new SqlConnection(connectionString))
+			{
+				try
+				{
+					SqlCommand command = new SqlCommand(selectSql, connection);
+					await command.Connection.OpenAsync();
+					SqlDataReader reader = await command.ExecuteReaderAsync();
+					while (await reader.ReadAsync())
+					{
+						int holdId = reader.GetInt32("HoldId");
+						string holdnavn = reader.GetString("Holdnavn");
+						string instruktørnavn = reader.GetString("Instruktørnavn");
+						DateOnly startDato = DateOnly.FromDateTime(reader.GetDateTime(reader.GetOrdinal("StartDato")));
+						DateOnly slutDato = DateOnly.FromDateTime(reader.GetDateTime(reader.GetOrdinal("SlutDato")));
+						string tid = reader.GetString("Tid");
+						string sted = reader.GetString("Sted");
+						double pris = reader.GetDouble("Pris");
+						int maxMedlemstal = reader.GetInt32("MaxMedlemstal");
+						Hold hold = new Hold(holdId, holdnavn, instruktørnavn, startDato, slutDato, tid, sted, pris, maxMedlemstal);
+						if (!holdListe.Contains(holdListe.Find(h => h.Holdnavn == holdnavn)))
+						{
+							holdListe.Add(hold);
+						}
+						else
+						{
+							throw new Exception("Det angivede holdnavn findes allerede i listen og kan ikke tilføjes");
+						}
+					}
+					reader.Close();
+				}
+				catch (SqlException sqlExp)
+				{
+					Console.WriteLine("Database fejl: " + sqlExp.Message);
+					throw sqlExp;
+				}
+				catch (Exception ex)
+				{
+					Console.WriteLine("Generel fejl: " + ex.Message);
+					throw ex;
+				}
+				finally
+				{
+
+				}
+			}
+			return holdListe;
 		}
 
-		public Task<Hold> GetHoldByIdAsync(int holdId)
+		public async Task<Hold> GetHoldByIdAsync(int holdId)
 		{
-			throw new NotImplementedException();
+			using (SqlConnection connection = new SqlConnection(connectionString))
+			{
+				Hold hold = null;
+				try
+				{
+					SqlCommand command = new SqlCommand(selectSql + " WHERE HoldId = @HoldId", connection);
+					await command.Connection.OpenAsync();
+					command.Parameters.AddWithValue("@BrugerId", holdId);
+					SqlDataReader reader = await command.ExecuteReaderAsync();
+					if (await reader.ReadAsync())
+					{
+						string holdnavn = reader.GetString("Holdnavn");
+						string instruktørnavn = reader.GetString("Instruktørnavn");
+						DateOnly startDato = DateOnly.FromDateTime(reader.GetDateTime(reader.GetOrdinal("StartDato")));
+						DateOnly slutDato = DateOnly.FromDateTime(reader.GetDateTime(reader.GetOrdinal("SlutDato")));
+						string tid = reader.GetString("Tid");
+						string sted = reader.GetString("Sted");
+						double pris = reader.GetDouble("Pris");
+						int maxMedlemstal = reader.GetInt32("MaxMedlemstal");
+						hold = new Hold(holdId, holdnavn, instruktørnavn, startDato, slutDato, tid, sted, pris, maxMedlemstal);
+					}
+					reader.Close();
+					return hold;
+				}
+				catch (SqlException sqlex)
+				{
+					Console.WriteLine(sqlex.Message);
+				}
+				catch (Exception ex)
+				{
+					Console.WriteLine(ex.Message);
+				}
+				finally
+				{
+
+				}
+				return hold;
+			}
 		}
 	}
 }
